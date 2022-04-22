97556dc27ec53b0e6e568cf9d87c5e7ca61e4372
Baptiste7512 <80527980+Baptiste7512@users.noreply.github.com>
2022-04-22 12:07:33 +0200

    create devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'skatespots#index'
   resources :skatespots, only: [:index, :show, :create, :new, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
