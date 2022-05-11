660ff090373e3119ea6b893f6ef6835a4f22d2a1
Baptiste7512 <80527980+Baptiste7512@users.noreply.github.com>
2022-05-11 15:16:50 +0200

    create model seed for admin + skatespot (photo name address)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
+require 'open-uri'
 
-
-puts "Destroy all User"
+puts "Destroy all Users"
 User.destroy_all
+puts "Destroy all Skatespots"
+Skatespot.destroy_all
 
 puts "Create admin..."
 
@@ -11,4 +13,13 @@ admin = User.create(
   password: "adminadmin"
 )
 
+puts "Create Skatespot"
+
+skatespot = Skatespot.create(
+  address: "Parc de bercy, Paris",
+  name: "Bercy la famille"
+)
+photo = URI.open("https://res.cloudinary.com/baptistev/image/upload/v1652198264/development/7xwgk7d3qud72v9h30dnpkrvil3l.jpg")
+skatespot.photo.attach(io: photo, filename: 'a.png', content_type: 'image/png')
+skatespot.save
 puts "Finish"
