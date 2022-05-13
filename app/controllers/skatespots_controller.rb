78223267b38f466b4877d4ec74c308d219a86229
baptiste vigneron <vigneron.baptiste@live.fr>
2022-05-13 11:59:32 +0200

    seed 4 skatespot

--- app/controllers/skatespots_controller.rb
+++ app/controllers/skatespots_controller.rb
@@ -1,5 +1,5 @@
 class SkatespotsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index]
   def index
     @skatespots = Skatespot.all
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,64 @@
 require 'open-uri'
 
+puts "Destroy all Conversation"
+Conversation.destroy_all
+
 puts "Destroy all Users"
 User.destroy_all
+
 puts "Destroy all Skatespots"
 Skatespot.destroy_all
 
 puts "Create admin..."
 
 admin = User.create(
-  username: "Starfoulah",
+  username: "Baptiste",
   email: "admin@gmail.com",
   password: "adminadmin"
 )
 
-puts "Create Skatespot"
+puts "Create Skatespot Bercy"
 
 skatespot = Skatespot.create(
   address: "Parc de bercy, Paris",
   name: "Bercy la famille"
 )
 photo = URI.open("https://res.cloudinary.com/baptistev/image/upload/v1652198264/development/7xwgk7d3qud72v9h30dnpkrvil3l.jpg")
+skatespot.photo.attach(io: photo, filename: 'a.png', content_type: 'image/png')
+skatespot.save
+puts "Create Skatespot Republique"
+skatespot = Skatespot.create(
+  address: "9 Place de la République, Paris",
+  name: "Repulife"
+)
+photo = URI.open("https://res.cloudinary.com/baptistev/image/upload/v1652198450/development/cyiectcx1p48s3gakzyzqcoz110g.jpg")
+skatespot.photo.attach(io: photo, filename: 'a.png', content_type: 'image/png')
+skatespot.save
+
+puts "Create Skatespot Pantin"
+skatespot = Skatespot.create(
+  address: "Place de la Pointe, 93500, Pantin",
+  name: "Skatepark de Pantin"
+)
+photo = URI.open("https://res.cloudinary.com/baptistev/image/upload/v1652435176/development/Skatepark_Pantin_2_uqs4o4.jpg")
+skatespot.photo.attach(io: photo, filename: 'a.png', content_type: 'image/png')
+skatespot.save
+
+puts "Create Skatespot Arcueil"
+skatespot = Skatespot.create(
+  address: "58 Avenue François Vincent Raspail, 94110 Arcueil, France",
+  name: "Arcueil"
+)
+photo = URI.open("https://res.cloudinary.com/baptistev/image/upload/v1652435366/development/Skatepark_bowl_Arcueil_lggzkl.jpg")
+skatespot.photo.attach(io: photo, filename: 'a.png', content_type: 'image/png')
+skatespot.save
+
+puts "Fac diderot"
+skatespot = Skatespot.create(
+  address: "Esplanade Pierre Vidal-Naquet, 75013 Paris, France",
+  name: "Diderot"
+)
+photo = URI.open("https://res.cloudinary.com/baptistev/image/upload/v1650634480/development/rxn3b7oprcz76v8w4h18io8wp0pq.jpg")
 skatespot.photo.attach(io: photo, filename: 'a.png', content_type: 'image/png')
 skatespot.save
 puts "Finish"
