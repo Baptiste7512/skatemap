48b7b8b7241f5f6e7184294a155917d985edbe12
baptiste vigneron <vigneron.baptiste@live.fr>
2022-07-18 15:58:18 +0200

    add button photo

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,6 @@
 class Conversation < ApplicationRecord
   belongs_to :skatespot
   belongs_to :user
-  validates :content, length: { minimum: 5 }
+  validates :content, length: { minimum: 1 }
   has_one_attached :photo
 end
